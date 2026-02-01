f4ede2798287b8dee68a8059de80c991320e75f2
zullsrgl <zulal.sariogluu@gmail.com>
2026-02-01 11:14:03 +0300

    Initial Commit

---
+++ SubWiser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SubWiser
+//
+//  Created by Enes Arabacı on 1.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SubWiser/SubWiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubWiserApp.swift
+//  SubWiser
+//
+//  Created by Enes Arabacı on 1.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SubWiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
